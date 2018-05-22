3d0c5eee6ece81a1642c89fb98ce7aca13c82081
admin <faraz.ismail510@gmail.com>
2018-05-22 09:23:23 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var router = express.Router();
+
+//load controller code dealing with database mongodb and Routes collection
+var controllerMongoCollection = require('../controllers/database');
+
 //to process data sent in on request need body-parser module
 var bodyParser = require('body-parser');
 var path = require('path'); //to work with separtors on any OS including Windows
 var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-router.post('/storeData', function(req, res,next) {
-    var value_name = req.body.order;
-    res.send("order successfully received: " + value_name);
-});
+
+// Our storeData POST endpoint to save the data from the order
+router.post('/storeData', controllerMongoCollection.storeData);
+
+// Our getAllOrders GET endpoint (for testing)
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
+
+// Our getAllOrders POST endpoint
+router.post('/getAllOrders', controllerMongoCollection.getAllOrders);
+
 module.exports = router;
